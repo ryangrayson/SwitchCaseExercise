b5293da4de2c5d29e31a4ec2bcbc0c50de7341cf
Ryan Grayson <ryan.g.grayson@gmail.com>
2020-08-25 14:11:34 -0500

    basic exercise completed

---
+++ SwitchCaseExercise/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SwitchCaseExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who is your favorite motorcycle manufacturer?");
+
+            var motoManu = Console.ReadLine();
+
+        switch (motoManu.ToLower())
+            {
+                case "kawasaki":
+
+                case "honda":
+
+                case "yamaha":
+                    Console.WriteLine("That ain't a Harley");
+                    break;
+                case "harley-davidson":
+                    Console.WriteLine("Hell yeah, brother. VROOM VROOM");
+                    break;
+                case "indian":
+                    Console.WriteLine("HAHAHAHAHAHAHAHAHA");
+                    break;
+                default:
+                    Console.WriteLine("Never heard of em");
+                    break;
+
+            }
+        }
+    }
+}
